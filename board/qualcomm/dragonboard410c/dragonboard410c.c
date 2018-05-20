cda35de345ce26407159bf70e3cc14efb916ced8
Ramon Fried <ramon.fried@gmail.com>
2018-05-20 22:00:41 +0300

    ehci: msm: Replace board_prepare_usb with board_usb_init
    Use standard board_usb_init() instead of the specific board_prepare_usb.
    
    Signed-off-by: Ramon Fried <ramon.fried@gmail.com>

--- board/qualcomm/dragonboard410c/dragonboard410c.c
+++ board/qualcomm/dragonboard410c/dragonboard410c.c
@@ -41,7 +41,7 @@ int dram_init_banksize(void)
 	return 0;
 }
 
-int board_prepare_usb(enum usb_init_type type)
+int board_usb_init(int index, enum usb_init_type init)
 {
 	static struct udevice *pmic_gpio;
 	static struct gpio_desc hub_reset, usb_sel;
@@ -90,7 +90,7 @@ int board_prepare_usb(enum usb_init_type type)
 		}
 	}
 
-	if (type == USB_INIT_HOST) {
+	if (init == USB_INIT_HOST) {
 		/* Start USB Hub */
 		dm_gpio_set_dir_flags(&hub_reset,
 				      GPIOD_IS_OUT | GPIOD_IS_OUT_ACTIVE);
