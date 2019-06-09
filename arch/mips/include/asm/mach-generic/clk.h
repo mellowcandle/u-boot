8529ca948ecf84379e281f4cff33a8a30aa540c3
Ramon Fried <rfried.dev@gmail.com>
2019-06-09 07:41:20 +0300

    MIPS: add empty arch/clk.h
    The file is needed for compilation of various
    drivers (IE. macb).
    Add empty implementation so compilation succeeds.
    
    Signed-off-by: Ramon Fried <rfried.dev@gmail.com>

---
+++ arch/mips/include/asm/mach-generic/clk.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) Ramon Fried <rfried.dev@gmail.com>
+ */
+
+#ifndef __ASM_MIPS_ARCH_CLK_H
+#define __ASM_MIPS_ARCH_CLK_H
+
+/* Note: This is a placeholder header for driver compilation. */
+
+#endif
